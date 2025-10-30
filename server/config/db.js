41c7b8fc778ab9c8de09a5b3801dc0f5c43ebadc
Reen <maureenmukami531@gmail.com>
2025-10-30 14:20:41 +0300

    Add MongoDB connection setup in db.js

---
+++ server/config/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const uri = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(uri);
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
